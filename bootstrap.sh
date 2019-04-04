f25a493d83acd6f9ceeba562c3709d5c444d6591
jm-vasquez <jm.vasquez.camacho@gmail.com>
2019-04-03 21:09:23 -0600

    removed php5 setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,12 +13,19 @@ service apache2 reload
 
 # Add repo for latest PHP
 sudo add-apt-repository -y ppa:ondrej/php
-apt-get update -y
 sudo apt-get install software-properties-common -y
 
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu xenial main'
+apt-get update -y
+
+# Setting MySQL root user password root/root
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y mariadb-server mariadb-client
 
 # Install the Rest
-sudo apt-get install -y php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mcrypt php5.6-xdebug php5.6-mysql php5.6-dom php5.6-cli php5.6-json php5.6-common php5.6-mbstring php5.6-opcache php5.6-readline
+sudo apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-curl php7.1-gd php7.1-mcrypt php7.1-xdebug php7.1-mysql php7.1-dom php7.1-cli php7.1-json php7.1-common php7.1-mbstring php7.1-opcache php7.1-readline php7.1-soap
 sudo a2enmod rewrite
 sudo a2enmod headers
 sudo a2enmod ssl
